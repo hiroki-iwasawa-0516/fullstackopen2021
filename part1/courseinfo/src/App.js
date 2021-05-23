261dba0131d28615792a63e94096ba319a910709
Hiroki Iwasawa <hiroki.iwasawa@gmail.com>
2021-05-23 21:55:54 +0900

    step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,11 +6,21 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>
-      {props.parts} {props.exercises}
+      {props.name} {props.exercises}
     </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
+    </div>
   )
 }
 
@@ -34,9 +44,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content parts={part1} exercises={exercises1} />
-      <Content parts={part2} exercises={exercises2} />
-      <Content parts={part3} exercises={exercises3} />
+      <Content part1={part1} part2={part2} part3={part3}
+               exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   )
