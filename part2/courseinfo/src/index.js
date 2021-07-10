57c9b87b15966722ebd8d5808d53597cd4a633f0
Hiroki Iwasawa <hiroki.iwasawa@gmail.com>
2021-07-10 17:38:39 +0900

    2.2: Course information step7 & 2.3: Course information step8

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -8,7 +8,7 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  const sum = course.parts.reduce((s, p) => s + p.exercises, 0)
   return(
     <p>Number of exercises {sum}</p>
   ) 
@@ -37,6 +37,7 @@ const Course =({ course }) => {
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
