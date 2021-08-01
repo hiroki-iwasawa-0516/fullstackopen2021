a3854b20f83d8c4b39daba19ab9a44a3957c47d4
Hiroki Iwasawa <hiroki.iwasawa@gmail.com>
2021-08-01 17:56:05 +0900

    3.16: Phonebook database, step4

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -13,6 +13,17 @@ app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.json())
 
+const errorHandler = (error, request, response, next) => {
+    console.error("aaaa")
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
 let persons = [
     {
       "id": 1,
@@ -53,7 +64,7 @@ app.get('/api/persons/:id', (request, response) => {
     }
   })
 
-  app.delete('/api/persons/:id', (request, response) => {
+  app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
       .then(result => {
         response.status(204).end()
@@ -86,6 +97,14 @@ app.get('/info', (request, response) => {
     response.end()
 
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 // bind ip adress for wsl2
