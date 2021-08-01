9e0803b09025ad2bf786101a4379aeb2ea43f55c
Hiroki Iwasawa <hiroki.iwasawa@gmail.com>
2021-08-01 19:06:10 +0900

    3.22: Lint configuration

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -14,46 +14,46 @@ app.use(morgan('tiny'))
 app.use(express.json())
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
-})
-
-app.post('/api/persons', (request, response, next) => {
-    const person = new Person({
-      name: request.body.name,
-      number: request.body.number
-    })
-
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
+app.post('/api/persons', (request, response, next) => {
+  const person = new Person({
+    name: request.body.name,
+    number: request.body.number
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+    .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
@@ -63,5 +63,5 @@ app.use(errorHandler)
 const PORT = process.env.PORT
 // bind ip adress for wsl2
 app.listen(PORT, '0.0.0.0',  () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
