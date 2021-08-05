e02154d9dd1e860af2c66dcb7e7029cc8b5ee153
Hiroki Iwasawa <hiroki.iwasawa@gmail.com>
2021-08-05 22:14:08 +0900

    4.10: Blog list tests, step3

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -20,6 +20,26 @@ test('all notes are returned', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length +1)
+  expect(response.body).toContainEqual({ ...newBlog, __v: expect.any(Number), _id: expect.any(String) })
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
