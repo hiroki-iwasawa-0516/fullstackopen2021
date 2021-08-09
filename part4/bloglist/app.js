b3d65f50122d0987c6727c289b1935c9833ba506
Hiroki Iwasawa <hiroki.iwasawa@gmail.com>
2021-08-09 15:25:28 +0900

    4.18: bloglist expansions, step6

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 
 const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
 const usersRouter = require('./controllers/users')
 
 const cors = require('cors')
@@ -15,6 +16,7 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, us
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 
 module.exports = app
