a37a401ef0e10627f531a665e31fd5eda8cb7466
Hiroki Iwasawa <hiroki.iwasawa@gmail.com>
2021-08-14 16:36:26 +0900

    5.13: Blog list tests, step1

---
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+import Blog from './Blog'
+
+test('renders blog', () => {
+  const blog = {
+    title: 'title',
+    author: 'author',
+    url: 'url',
+    likes: 0
+  }
+
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+  expect(component.container).toHaveTextContent(
+    'title'
+  )
+  expect(component.container).toHaveTextContent(
+    'author'
+  )
+  expect(component.container).not.toHaveTextContent(
+    'url'
+  )
+  expect(component.container).not.toHaveTextContent(
+    '0'
+  )
+})
