a41db317ab1a8546455c39185df955055a5df0f6
Hiroki Iwasawa <hiroki.iwasawa@gmail.com>
2021-08-14 21:00:21 +0900

    5.18: bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,14 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +16,23 @@ describe('Blog app', function() {
     cy.contain('log in to application')
     cy.contain('username')
     cy.contain('password')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
+    })
   })
 })
