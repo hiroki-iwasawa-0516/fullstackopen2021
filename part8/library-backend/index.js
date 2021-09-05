74af9cb45bcdb2de0787acb6d87710a104a6492f
Hiroki Iwasawa <hiroki.iwasawa@gmail.com>
2021-09-05 22:16:28 +0900

    8.7: updating the birth yearof an author

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -118,6 +118,11 @@ const typeDefs = gql`
       published: Int!
       genres: [String!]!
     ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -162,6 +167,17 @@ const resolvers = {
       }
 
       return book
+    },
+
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = {...author, born: args.setBornTo}
+      authors = authors.map(a => a.name === updatedAuthor.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
